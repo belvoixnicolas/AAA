94ca4dd411587e4efd371ba1620a1546f6c1efcd
nicoP08 <pnsdaaf@gmail.com>
2018-06-19 17:11:59 +0200

    1

---
+++ fiche_voiture.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Voiture</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+<body>
+  <header>
+  </header>
+
+  <main>
+    <div class="photo_voiture">
+      <img class="voiture" src="./img/voiture1.png" alt="voiture" />
+    </div>
+
+
+		<div id="blocInfos">
+
+      <div class="ficheMarque">Marque</div>
+      <div class="ficheModèle">Modèle</div>
+      <div class="fichePrix">Prix € </div>
+		</div>
+		<div id="blocDescription">
+      <div class="ficheAnnee">Année mise en circulation</div>
+      <div class="ficheKilometres">Kilométrage</div>
+      <div class="ficheCouleur">Couleur</div>
+		</div>
+
+      <p ="ficheDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+  </main>
+
+  <footer>
+  </footer>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
