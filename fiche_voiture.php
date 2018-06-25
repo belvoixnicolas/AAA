c0c599fdefa1c27540407b5eb68ec75b4aa295ea
nicoP08 <pnsdaaf@gmail.com>
2018-06-25 14:26:58 +0200

    maj fiche voiture

--- fiche_voiture.php
+++ fiche_voiture.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Voiture</title>
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/fiche_voiture.css">
 	<link rel="stylesheet" type="text/css" href="css/nav.css">
 </head>
 
@@ -15,28 +15,24 @@
 
   <main>
     <div class="photo_voiture">
-      <img class="voiture" src="./img/voiture1.png" alt="voiture" />
+      <img class="voiture" src="img/voiture1.png" alt="voiture" />
     </div>
 
+				<div>
+					<div id="blocInfos">
+      		<div class="ficheTitre">Titre</div>
+      		<div class="fichePrix">Prix € </div>
+				</div>
+				<div id="blocDescription">
+					<ul>
+      		<li class="ficheAnnee">Année mise en circulation</li>
+      		<li class="ficheKilometres">Kilométrage</li>
+      		<li class="ficheCouleur">Couleur</li>
+				</ul>
+			</div>
 
-		<div id="blocInfos">
-
-      <div class="ficheMarque">Marque</div>
-      <div class="ficheModèle">Modèle</div>
-      <div class="fichePrix">Prix € </div>
-		</div>
-		<div id="blocDescription">
-      <div class="ficheAnnee">Année mise en circulation</div>
-      <div class="ficheKilometres">Kilométrage</div>
-      <div class="ficheCouleur">Couleur</div>
-		</div>
-
-      <p ="ficheDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-  </main>
-
-  <footer>
-  </footer>
-  <script src="js/scripts.js"></script>
-</body>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</main>
+		<?php include 'include/footer.html'; ?>
+	</body>
 </html>
