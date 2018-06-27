ed8763ffde4e10614e6511d18f95ea05db665cbc
Alexsansdraps <a.porhansl@icloud.com>
2018-06-27 14:57:49 +0200

    étoiles

--- index.php
+++ index.php
@@ -1,92 +1,5 @@
 <!DOCTYPE html>
 <html lang="fr">
-<<<<<<< HEAD
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="icon" type="image/x-icon" href="favicon.ico">
-		<link rel="shortcut icon" type="image/x-icon" href="resource/img/favicon.ico">
-		<link rel="stylesheet" type="text/css" href="css/reset.css">
-		<link rel="stylesheet" type="text/css" href="css/nav.css">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<link rel="stylesheet" type="text/css" href="css/footer.css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-		<title>Agence Automobile Ardennaise</title>
-	</head>
-	<body>
-		<?php include 'include/navbar.html'; ?>
-		<header>
-			<button type="button" name="button1" id="acheter" class="bouton">ACHETER</button>
-			<button type="button" name="button2" id="vendre" class="bouton">VENDRE</button>
-		</header>
-		<main>
-			<section id="presentation">
-				<div id="img">
-					<img src="img/voiture1.png" alt="">
-				</div>
-				<div id="zonetexte">
-					<p>
-						Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis ut conscius ductus est aliique congregati sunt ex diversis civitatibus multi, qui atrocium criminum ponderibus urgebantur.Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte , incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris
-					</p>
-				</div>
-			</section>
-			<section id="temoin">
-				<h2>Nos clients témoignent</h2>
-				<div id="avis">
-					<div class="temoignage">
-						<div class="pasclasse">
-							<div class="">
-								<p>
-									<strong>Jean Neymar (75) : </strong>
-								</p>
-							</div>
-							<div class="zut">
-								<i class="fas fa-star"></i>
-								<i class="fas fa-star"></i>
-								<i class="fas fa-star"></i>
-								<i class="fas fa-star"></i>
-								<i class="fas fa-star"></i>
-							</p>
-						</div>
-					</div>
-					<div class="">
-						<p>
-							"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis ut conscius ductus est aliique congregati sunt ex diversis civitatibus multi, qui atrocium criminum ponderibus urgebantur"
-						</p>
-					</div>
-				</div>
-				<div class="temoignage">
-					<p>
-						Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis ut conscius ductus est aliique congregati sunt ex diversis civitatibus multi, qui atrocium criminum ponderibus urgebantur
-					</p>
-				</div>
-				<div class="temoignage">
-					<p>
-						Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis ut conscius ductus est aliique congregati sunt ex diversis civitatibus multi, qui atrocium criminum ponderibus urgebantur
-					</p>
-				</div>
-				<div class="temoignage">
-					<p>
-						Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis ut conscius ductus est aliique congregati sunt ex diversis civitatibus multi, qui atrocium criminum ponderibus urgebantur
-					</p>
-				</div>
-			</div>
-		</section>
-		<section id="partenaires">
-			<h2>Nos partenaires</h2>
-			<div class="container">
-				<div class="logo"></div>
-				<div class="logo"></div>
-				<div class="logo"></div>
-				<div class="logo"></div>
-				<div class="logo"></div>
-			</div>
-		</section>
-	</main>
-	<?php include 'include/footer.html'; ?>
-</body>
-=======
   <head>
     <meta charset="utf-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -98,6 +11,9 @@
     <link rel="stylesheet" type="text/css" href="css/nav.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
       <link rel="stylesheet" type="text/css" href="css/footer.css">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+
   <title>Agence Automobile Ardennaise</title>
   </head>
 
@@ -129,7 +45,12 @@
               <p><strong>Jean Neymar (75):</strong></p>
             </div>
             <div class="zut">
-              <img src="img/goldenstar.png" alt=""><img src="img/goldenstar.png" alt=""><img src="img/goldenstar.png" alt=""><img src="img/goldenstar.png" alt=""><img src="img/goldenstar.png" alt=""></p>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+
             </div>
 
           </div>
@@ -166,5 +87,4 @@
     <?php include 'include/footer.html'; ?>
 
   </body>
->>>>>>> 947c66a1a1dfed43768b432803320e126cd88896
 </html>
