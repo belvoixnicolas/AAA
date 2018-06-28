165f0e18c9899f20c211524c9c5cb96230cebeee
greglab74 <greg.labbe08@gmail.com>
2018-06-28 14:54:32 +0200

    g

---
+++ liste_voiture.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="resource/img/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/nav.css">
+    <link rel="stylesheet" type="text/css" href="css/style_liste_voiture.css">
+      <link rel="stylesheet" type="text/css" href="css/footer.css">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+
+  <title>Agence Automobile Ardennaise</title>
+  </head>
+
+  <body>
+    <?php include 'include/navbar.html'; ?>
+    <header>
+
+    </header>
+    <main>
+      <div id="phrases">
+
+      </div>
+      <div id="container">
+        <section>
+
+        </section>
+        <div id="corps">
+
+        </div>
+      </div>
+
+    </main>
+    <?php include 'include/footer.html'; ?>
+  </body>
+</html>
