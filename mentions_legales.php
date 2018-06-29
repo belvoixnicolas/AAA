737c8c879c0dbb366385ec75b532bbf399b211b9
nicoP08 <pnsdaaf@gmail.com>
2018-06-29 14:09:20 +0200

    mentions legalles

---
+++ mentions_legales.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="resource/img/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/nav.css">
+    <link rel="stylesheet" type="text/css" href="css/mentions_legales.css"
+      <link rel="stylesheet" type="text/css" href="css/footer.css">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+
+  <title>Mentions legales</title>
+  </head>
+
+  <body>
+    <?php include 'include/navbar.html'; ?>
+    <header>
+    <h2 class="titre_mentions">Mentions légales</h2>
+</header>
+
+<body>
+  <ul>
+    <li>Raison sociale</li>
+    <li>Siège social</li>
+    <li>Téléphone</li>
+    <li>Directeur de publication</li>
+    <li>RCS</li>
+    <li>Nom de l'hébergeur</li>
+    <li>Adresse de l'hébergeur</li>
+    <li>Numéro de téléphone de l'hébergeur</li>
+  </ul>
+
+
+
+  </body>
+<footer>
+  <?php include 'include/footer.html'; ?>
+</html>
