a324eeada8e1c025afcebd70019a1fb154a49f3d
nicoP08 <pnsdaaf@gmail.com>
2018-07-02 15:00:35 +0200

    form vente

--- vendre.php
+++ vendre.php
@@ -16,11 +16,13 @@
       <?php include 'include/navbar.html'; ?>
 
       <main>
-        <h2>VENTE</h2>
+        <h2>VENDRE</h2>
 
         <form action="#" method="post">
           <fieldset>
+
             <p class="formTitres">Vos coordonnées</p>
+            <p class="textform">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
             <div>
               <label for="name">Votre nom : <span class="red">*</span></label>
@@ -42,7 +44,7 @@
               <input type="phone" id="tel" class="mefform" name="phone" placeholder="00 00 00 00 00">
             </div>
         </fieldset>
-      </main
+      </main>
       <?php include 'include/footer.html'; ?>
     </body>
   </html>
