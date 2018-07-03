a7e2dd0705b4ae693bfe72df8841fca12986983e
Alexsansdraps <a.porhansl@icloud.com>
2018-07-03 14:34:16 +0200

    c

--- index.php
+++ index.php
@@ -1,154 +1,161 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
-    <link rel="shortcut icon" type="image/x-icon" href="resource/img/favicon.ico">
-    <link rel="stylesheet" type="text/css" href="css/reset.css">
-    <link rel="stylesheet" type="text/css" href="css/nav.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-      <link rel="stylesheet" type="text/css" href="css/footer.css">
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js%22%3E"></script>
-
-  <title>Agence Automobile Ardennaise</title>
-</head>
-
-  <body>
-    <?php include 'include/navbar.html'; ?>
-    <header id="header">
-      <button type="button" name="button1" id="acheter" class="bouton">ACHETER</button>
-        <button type="button" name="button2" id="vendre" class="bouton">VENDRE</button>
-
-
-    </header>
-    <main>
-      <section id="presentation">
-        <div id="img">
-          <img src="img/voiture1.png" alt="">
-  </div>
-        <div id="zonetexte">
-            <p>Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis ut conscius ductus est aliique congregati sunt ex diversis civitatibus multi, qui atrocium criminum ponderibus urgebantur.Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte , incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris</p>
-        </div>
-    </section>
-    <section>
-      <div id="faq">
-
-      </div>
-    </section>
-    <section id="temoin">
-      <h2>Nos clients témoignent</h2>
-      <div id="avis">
-        <div class="temoignage">
-          <div class="pasclasse">
-            <div class="test">
-              <div class="centre">
-                <img src="./img/images.jpeg" id="homer" alt="">
-              </div>
-              <div class="centre">
-                <p><strong>Sarah V. (35):</strong></p>
-                <p><i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i></p>
-              </div>
-
-            </div>
-            <div class="zut">
-              <p>"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis  "</p>
-            </div>
-          </div>
-
-        </div>
-        <div class="temoignage">
-          <div class="pasclasse">
-            <div class="test">
-              <div class="">
-                <img src="./img/fed1.jpg" id="homer" alt="">
-              </div>
-              <div class="centre">
-                <p><strong>Roger F (Suisse):</strong></p>
-                <p><i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i></p>
-              </div>
-
-            </div>
-            <div class="zut">
-              <p>"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis  "</p>
-            </div>
-          </div>
-
-        </div>
-
-        <div class="temoignage">
-          <div class="pasclasse">
-            <div class="test">
-              <div class="">
-                <img src="./img/anne.jpeg" id="homer" alt="">
-              </div>
-              <div class="centre">
-                <p><strong>Anne H. (51):</strong></p>
-                <p><i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i></p>
-              </div>
-
-            </div>
-            <div class="zut">
-              <p>"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis  "</p>
-            </div>
-          </div>
-
-        </div>
-        <div class="temoignage">
-          <div class="pasclasse">
-            <div class="test">
-              <div class="">
-                <img src="./img/face1.jpeg" id="homer" alt="">
-              </div>
-              <div class="centre">
-                <p><strong>David S. (08):</strong></p>
-                <p><i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i></p>
-              </div>
-
-            </div>
-            <div class="zut">
-              <p>"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis  "</p>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </section>
-
-      <section id="partenaires">
-        <h2>Nos partenaires</h2>
-      <div class="container">
-        <img src="./img/logoaaa.jpg" class="logo" alt="">
-        <img src="./img/logoaaa.jpg" class="logo" alt="">
-        <img src="./img/logoaaa.jpg" class="logo" alt="">
-        <img src="./img/logoaaa.jpg" class="logo" alt="">
-        <img src="./img/logoaaa.jpg" class="logo" alt="">
-      </div>
-    </section>
-
-    </main>
-    <?php include 'include/footer.html'; ?>
-
-<script type="text/javascript" src="js/script.js">
-
-</script>
-  </body>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" type="image/x-icon" href="favicon.ico">
+		<link rel="shortcut icon" type="image/x-icon" href="resource/img/favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/reset.css">
+		<link rel="stylesheet" type="text/css" href="css/nav.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/footer.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js%22%3E"></script>
+		<title>Agence Automobile Ardennaise</title>
+	</head>
+	<body>
+		<?php include 'include/navbar.html'; ?>
+		<header id="header">
+			<button type="button" name="button1" id="acheter" class="bouton">ACHETER</button>
+			<button type="button" name="button2" id="vendre" class="bouton">VENDRE</button>
+		</header>
+		<main>
+			<section id="presentation">
+				<div id="img">
+					<img src="img/voiture1.png" alt="">
+				</div>
+				<div id="zonetexte">
+					<p>
+						Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis ut conscius ductus est aliique congregati sunt ex diversis civitatibus multi, qui atrocium criminum ponderibus urgebantur.Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte , incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris
+					</p>
+				</div>
+			</section>
+			<section>
+				<div id="faq"></div>
+			</section>
+			<section id="temoin">
+				<h2>Nos clients témoignent</h2>
+				<div id="avis">
+					<div class="temoignage">
+						<div class="pasclasse">
+							<div class="test">
+								<div class="centre">
+									<img src="./img/images.jpeg" id="homer" alt="">
+								</div>
+								<div class="centre">
+									<p>
+										<strong>Sarah V. (35):</strong>
+									</p>
+									<p>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+									</p>
+								</div>
+							</div>
+							<div class="zut">
+								<p>
+									"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis  "
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="temoignage">
+						<div class="pasclasse">
+							<div class="test">
+								<div class="">
+									<img src="./img/fed1.jpg" id="homer" alt="">
+								</div>
+								<div class="centre">
+									<p>
+										<strong>Roger F (Suisse):</strong>
+									</p>
+									<p>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+									</p>
+								</div>
+							</div>
+							<div class="zut">
+								<p>
+									"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis  "
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="temoignage">
+						<div class="pasclasse">
+							<div class="test">
+								<div class="">
+									<img src="./img/anne.jpeg" id="homer" alt="">
+								</div>
+								<div class="centre">
+									<p>
+										<strong>Anne H. (51):</strong>
+									</p>
+									<p>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+									</p>
+								</div>
+							</div>
+							<div class="zut">
+								<p>
+									"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis  "
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="temoignage">
+						<div class="pasclasse">
+							<div class="test">
+								<div class="">
+									<img src="./img/face1.jpeg" id="homer" alt="">
+								</div>
+								<div class="centre">
+									<p>
+										<strong>David S. (08):</strong>
+									</p>
+									<p>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+										<i class="fas fa-star"></i>
+									</p>
+								</div>
+							</div>
+							<div class="zut">
+								<p>
+									"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis  "
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="partenaires">
+				<h2>Nos partenaires</h2>
+				<div class="container">
+					<img src="./img/logoaaa.jpg" class="logo" alt="">
+					<img src="./img/logoaaa.jpg" class="logo" alt="">
+					<img src="./img/logoaaa.jpg" class="logo" alt="">
+					<img src="./img/logoaaa.jpg" class="logo" alt="">
+					<img src="./img/logoaaa.jpg" class="logo" alt="">
+				</div>
+			</section>
+		</main>
+		<?php include 'include/footer.html'; ?>
+		<script type="text/javascript" src="js/script.js">
+		</script>
+	</body>
 </html>
