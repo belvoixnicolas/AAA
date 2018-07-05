ba8f1cb87d03090857da2be383cd35fa3ddf2565
greglab74 <39262246+greglab74@users.noreply.github.com>
2018-07-05 19:46:52 +0200

    greg

--- js/onscroll.js
+++ js/onscroll.js
@@ -1,7 +1,16 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 250) {
+    if (scroll < 200) {
+        $("#animated1").addClass("cache");
+        $("#animated2").addClass("cache");
+        $("#animated3").addClass("cache");
+    }
+
+    if (scroll >= 200) {
+        $("#animated1").removeClass("cache");
+        $("#animated2").removeClass("cache");
+        $("#animated3").removeClass("cache");
         $("#animated1").addClass("animated bounceInRight");
         $("#animated2").addClass("animated2 bounceInRight2");
         $("#animated3").addClass("animated3 bounceInRight3");
