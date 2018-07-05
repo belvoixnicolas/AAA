9333ca475e88a599ee809b16bafd8a068a21c340
Alexsansdraps <a.porhansl@icloud.com>
2018-07-05 14:28:10 +0200

    v

--- vendre.php
+++ vendre.php
@@ -16,56 +16,38 @@
       <?php include 'include/navbar.html'; ?>
 
       <main>
-        <h2>VENDRE</h2>
+        <div class="container">
 
-        <form action="#" method="post">
-          <fieldset>
+          <div class="coordonnées">
+            <h3>Coordonnées</h3>
+            <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
 
-            <p class="formTitres">Vos coordonnées</p>
-            <p class="textform">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <div class="part1">
-              <input type="text" id="nom" class="mefform" name="name" placeholder="nom">
-            </div>
-
-            <div>
-              <input type="text" id="prenom" class="mefform" name="firstname" placeholder="prénom">
-            </div>
-
-            <div>
-              <input type="email" id="email" class="mefform" name="mail" placeholder="exemple@test.com">
-            </div>
-
-            <div>
-
-              <input type="phone" id="tel" class="mefform" name="phone" placeholder="00 00 00 00 00">
-            </div>
-
-
-        </fieldset>
-
-        <fieldset>
-
-          <p class="formTitres">La voiture</p>
-          <p class="textform">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <div class="part2">
-            <input type="text" id="marque" class="mefform" name="marque" placeholder="marque">
+            <form class="venteform" action="index.html" method="post">
+              <input type="radio" name="genre" placeholder="monsieur">Monsieur
+              <input type="radio" name="genre" placeholder="madame"> Madame
+              <input type="radio" name="genre" placeholder="autre">Autre
+              <br>
+              <input type="text" name="prenom" placeholder="Prénom"> <br>
+              <input type="text" name="nom" placeholder="Nom"> <br>
+              <input type="text" name="rue" placeholder="Rue"> <br>
+              <input type="text" name="cp" placeholder="Code postal"> <br>
+              <input type="text" name="ville" placeholder="Ville"> <br>
+              <input type="text" name="tel" placeholder="Téléphone"> <br>
+              <input type="email" name="" placeholder="E-mail"> <br>
+              <input type="submit" class="button" value"submit">
+            </form>
           </div>
-
-          <div>
-            <input type="text" id="modele" class="mefform" name="modele" placeholder="modèle">
+          <div class="voiture">
+            <h3>Véhicule</h3>
+            <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
           </div>
-
-          <div>
-            <input type="text" id="couleur" class="mefform" name="couleur" placeholder="couleur">
+          <div class="commentaires">
+            <h3>Commentaire</h3>
+            <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
           </div>
-
-          <div>
-            <input type="text" id="datemec" class="mefform" name="datemec" placeholder="Date de mise en circulation">
-          </div>
-      </fieldset>
-
-    </form>
+        </div>
       </main>
       <?php include 'include/footer.html'; ?>
+
     </body>
   </html>
