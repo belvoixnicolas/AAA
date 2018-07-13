a276b4bcf94fd639e42c49910c06f3826a671b83
greglab74 <greg.labbe08@gmail.com>
2018-07-13 16:03:37 +0200

    g

--- essai.php
+++ essai.php
@@ -1,11 +1,23 @@
 <?php
-try
-{
-	$bdd = new PDO('mysql:host=db745063290.db.1and1.com;dbname=db745063290;charset=utf8', 'db745063290', ' !Nicolas019');
-echo('Connexiion reussie')
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-}
+
+$serveur = 'greggl74.sql.free.fr';
+
+$login = 'greggl74';
+
+$mot_de_passe = 'Thomas1974';
+
+$nom_bd = 'greggl74';
+
+if(mysql_connect($serveur, $login, $mot_de_passe)) echo "ok";
+
+else echo "non";
+
+mysql_select_db($nom_bd);
+
+mysql_query("set names 'utf8'");
+
+$sql = INSERT INTO `marques` (`id` ,`nom`) VALUES (NULL , 'greg4');
+
+
+
 ?>
