57997068fbcf78aa2b00d12562d9c00c990d5cf9
greglab74 <greg.labbe08@gmail.com>
2018-07-13 16:24:02 +0200

    g

--- essai.php
+++ essai.php
@@ -8,16 +8,23 @@ $mot_de_passe = 'Thomas1974';
 
 $nom_bd = 'greggl74';
 
-if(mysql_connect($serveur, $login, $mot_de_passe)) echo "ok";
-
-else echo "non";
-
-mysql_select_db($nom_bd);
-
-mysql_query("set names 'utf8'");
-
-$sql = INSERT INTO `marques` (`id` ,`nom`) VALUES (NULL , 'greg4');
 
 
+try {
+    $conn = new PDO("mysql:host=$serveur;dbname=$nom_bd", $login, $mot_de_passe);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO marques (nom)
+    VALUES ('testpdo')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
 
 ?>
