a6be69ecd30a7c6484329262f62a8ecdd13bb1da
Alexsansdraps <a.porhansl@icloud.com>
2018-07-13 15:13:18 +0200

    v

--- vendre.php
+++ vendre.php
@@ -1,30 +1,31 @@
 <!DOCTYPE HTML>
 <html lang="fr">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Vente</title>
-		<link rel="icon" type="image/x-icon" href="favicon.ico">
-		<!--[if IE]>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Vente</title>
+	<link rel="icon" type="image/x-icon" href="favicon.ico">
+	<!--[if IE]>
 		<link rel="shortcut icon" type="image/x-icon" href="resource/img/favicon.ico" />
 		<![endif]-->
-		<link rel="stylesheet" type="text/css" href="css/reset.css">
-		<link rel="stylesheet" type="text/css" href="css/nav.css">
-		<link rel="stylesheet" type="text/css" href="css/footer.css">
-		<link rel="stylesheet" type="text/css" href="css/vendre.css">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous"
-		>
-	</head>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/nav.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+	<link rel="stylesheet" type="text/css" href="css/vendre.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+</head>
 
-	<body>
-		<?php include 'include/navbar.html'; ?>
+<body>
+	<?php include 'include/navbar.html'; ?>
 
-		<main>
-			<h1>Vendez votre véhicule avec simplicité et rapidité !</h1>
+	<main>
+		<h1>Vendez votre véhicule avec simplicité et rapidité !</h1>
 
-			<div class="container">
+		<div class="conteneur">
+			<div class="divdiv">
 				<div class="gauche">
 					<h4> <img src="./img/fast.png" alt=""> Rapide</h4>
 					<p class="p">
@@ -42,8 +43,10 @@
 					</p>
 					<br>
 				</div>
-				<div class="centre">
-					<form class="" action="index.html" method="post">
+			</div>
+			<form class="formvente" action="index.html" method="post">
+				<div class="droite">
+					<div class="divdansunediv">
 						<h2>Votre véhicule :</h2>
 						<input type="text" id="marque" name="marque" placeholder="Marque et modèle">
 						<br>
@@ -54,7 +57,7 @@
 						<input type="text" id="km" name="km" placeholder="Kilomètrage">
 						<br>
 					</div>
-					<div class="droite">
+					<div class="divdansunedivbis">
 						<h2>Vos coordonnées :</h2>
 						<input type="text" id="nom" name="nom" placeholder="Nom et prénom">
 						<br>
@@ -64,130 +67,131 @@
 						<br>
 						<input type="email" id="mail" name="mail" placeholder="E-mail">
 						<br>
-					</form>
-					<button type="submit">
-						Submit
-						<i class="fa fa-paper-plane" aria-hidden="true"></i>
-						<span class="bar"><span class="load"></span></button>
+						<button type="submit">
+									Submit
+									<i class="fa fa-paper-plane" aria-hidden="true"></i>
+									<span class="bar"><span class="load"></span></button>
+					</div>
+			</form>
+			</div>
+		</div>
+
+
+
+
+		<section id="temoin">
+			<div class="titletemoin">
+				<img src="./img/client.png" class="icone" alt="">
+				<h2>Nos clients témoignent</h2>
+			</div>
+
+			<div id="avis">
+				<div class="temoignage">
+					<div class="pasclasse">
+						<div class="test">
+							<div class="">
+								<img src="./img/images.jpeg" id="homer" alt="">
+							</div>
+							<div class="centre">
+								<p>
+									<strong>Sarah V. (35):</strong>
+								</p>
+								<p>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+								</p>
+							</div>
+						</div>
+						<div class="zut">
+							<p>
+								"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis "
+							</p>
+						</div>
 					</div>
 				</div>
-
-
-
-
-				<section id="temoin">
-					<div class="titletemoin">
-						<img src="./img/client.png" class="icone" alt="">
-						<h2>Nos clients témoignent</h2>
-					</div>
-
-					<div id="avis">
-						<div class="temoignage">
-							<div class="pasclasse">
-								<div class="test">
-									<div class="">
-										<img src="./img/images.jpeg" id="homer" alt="">
-									</div>
-									<div class="centre">
-										<p>
-											<strong>Sarah V. (35):</strong>
-										</p>
-										<p>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-										</p>
-									</div>
-								</div>
-								<div class="zut">
-									<p>
-										"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis  "
-									</p>
-								</div>
+				<div class="temoignage">
+					<div class="pasclasse">
+						<div class="test">
+							<div class="">
+								<img src="./img/fed1.jpg" id="homer" alt="">
+							</div>
+							<div class="centre">
+								<p>
+									<strong>Roger F (sw):</strong>
+								</p>
+								<p>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+								</p>
 							</div>
 						</div>
-						<div class="temoignage">
-							<div class="pasclasse">
-								<div class="test">
-									<div class="">
-										<img src="./img/fed1.jpg" id="homer" alt="">
-									</div>
-									<div class="centre">
-										<p>
-											<strong>Roger F (sw):</strong>
-										</p>
-										<p>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-										</p>
-									</div>
-								</div>
-								<div class="zut">
-									<p>
-										"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis  "
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class="temoignage">
-							<div class="pasclasse">
-								<div class="test">
-									<div class="">
-										<img src="./img/anne.jpeg" id="homer" alt="">
-									</div>
-									<div class="centre">
-										<p>
-											<strong>Anne H. (51):</strong>
-										</p>
-										<p>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-										</p>
-									</div>
-								</div>
-								<div class="zut">
-									<p>
-										"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis  "
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class="temoignage">
-							<div class="pasclasse">
-								<div class="test">
-									<div class="">
-										<img src="./img/face1.jpeg" id="homer" alt="">
-									</div>
-									<div class="centre">
-										<p>
-											<strong>David S. (08):</strong>
-										</p>
-										<p>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-											<i class="fas fa-star"></i>
-										</p>
-									</div>
-								</div>
-								<div class="zut">
-									<p>
-										"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis  "
-									</p>
-								</div>
-							</div>
+						<div class="zut">
+							<p>
+								"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis "
+							</p>
 						</div>
 					</div>
-				</section>
+				</div>
+				<div class="temoignage">
+					<div class="pasclasse">
+						<div class="test">
+							<div class="">
+								<img src="./img/anne.jpeg" id="homer" alt="">
+							</div>
+							<div class="centre">
+								<p>
+									<strong>Anne H. (51):</strong>
+								</p>
+								<p>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+								</p>
+							</div>
+						</div>
+						<div class="zut">
+							<p>
+								"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis "
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="temoignage">
+					<div class="pasclasse">
+						<div class="test">
+							<div class="">
+								<img src="./img/face1.jpeg" id="homer" alt="">
+							</div>
+							<div class="centre">
+								<p>
+									<strong>David S. (08):</strong>
+								</p>
+								<p>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+								</p>
+							</div>
+						</div>
+						<div class="zut">
+							<p>
+								"Quae dum ita struuntur, indicatum est apud Tyrum indumentum regale textum occulte, incertum quo locante vel cuius usibus apparatum. ideoque rector provinciae tunc pater Apollinaris eiusdem nominis "
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 
 
 
@@ -195,4 +199,5 @@
 
 	<?php include 'include/footer.html'; ?>
 </body>
+
 </html>
